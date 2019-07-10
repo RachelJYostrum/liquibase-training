734d694ce5665254ac9ac45d26ca48cd8c8f9804
Adam Johnston <ajohnston1219@gmail.com>
2019-07-10 11:08:05 -0500

    first commit

---
+++ liquibase-diff.bat
@@ -0,0 +1,4 @@
+liquibase diff ^
+--referenceUrl=jdbc:postgresql://reference-db.c15thsg7cjkl.us-east-1.rds.amazonaws.com/reference_db ^
+--referenceUsername=postgres ^
+--referencePassword=secretPassword
